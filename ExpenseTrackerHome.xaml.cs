02cbf01d0f62f3391dea4f5be7099c78a03699cc
sojman007 <sojman007@yahoo.com>
2020-03-25 04:08:43 +0100

    Add project files.

---
+++ ExpenseTrackerHome.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExpenseTrackerApp
+{
+    /// <summary>
+    /// Interaction logic for ExpenseTrackerHome.xaml
+    /// </summary>
+    public partial class ExpenseTrackerHome : Page
+    {
+        public ExpenseTrackerHome()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender , RoutedEventArgs args)
+        {
+            ExpenseReportPage reportPage = new ExpenseReportPage(peopleListBox.SelectedItem);
+            NavigationService.Navigate(reportPage);
+        }
+    }
+}
